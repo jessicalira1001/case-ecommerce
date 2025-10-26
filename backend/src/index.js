dfb2d6568c7244db9a322cec316b6522e1b1ff9d
jessicalira1001 <jessicalira1001@gmail.com>
2025-10-26 17:45:11 -0300

    feat(backend): Adicionar rotas do carrinho e de compras para serem usadas no servidor

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,8 @@ const express = require("express")
 const cors = require("cors");
 const app = express();
 const rotasProduto = require("./routes/produtosRoutes")
-
+const rotasCarrinho = require("./routes/carrinhoRoutes")
+const rotasCompra = require("./routes/compraRoutes")
 
 app.get('/',(req, res) => {
     res.send("Olá, Mundo");
@@ -11,6 +12,8 @@ app.get('/',(req, res) => {
 app.use(cors());
 app.use(express.json())
 app.use(rotasProduto)
+app.use(rotasCarrinho)
+app.use(rotasCompra)
 
 app.listen(process.env.PORT || 3001, () => {
     console.log("Servidor rodando na porta 3001")
