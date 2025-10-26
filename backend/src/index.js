af33772a4d8654d8f06c425209fdd6d808ebb06e
jessicalira1001 <jessicalira1001@gmail.com>
2025-10-25 23:59:39 -0300

    chore(backend): Configurar CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require("express")
+const cors = require("cors");
 const app = express();
 const rotasProduto = require("./routes/produtosRoutes")
 
@@ -7,10 +8,10 @@ const rotasProduto = require("./routes/produtosRoutes")
 app.get('/',(req, res) => {
     res.send("Olá, Mundo");
 });
-
+app.use(cors());
 app.use(express.json())
 app.use(rotasProduto)
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Servidor rodando na porta 3000")
+app.listen(process.env.PORT || 3001, () => {
+    console.log("Servidor rodando na porta 3001")
 })
